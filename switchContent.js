a37ce624151e3c8505f2c8e37ea0eac635f0dd27
David Duefrene <fortismilite@gmail.com>
2022-07-15 13:58:53 -0600

    Added some template animations & Started to adjust title/navebar

--- switchContent.js
+++ switchContent.js
@@ -1,11 +1,30 @@
-const switchContent = (event) => {
-	const target = event.target;
-	document.querySelector('.Nav-button.Active').classList.remove('Active');
+const switchContent = (target) => {
+	document.querySelector('.Title.Nav-button.Active').classList.remove('Active');
 	target.classList.add('Active')
 	document.querySelector('.Content-box.Active').classList.remove('Active');
 	document.querySelector(`#${target.id}-content`).classList.add('Active');
+	document.querySelector(`#${target.id}-content`).scrollIntoView();
 }
 
-document.querySelectorAll('.Nav-button').forEach(button => {
-	button.addEventListener('click', switchContent);
+const scrollObserverCallback = (entries) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			const target = entry.target;
+			switchContent(document.querySelector(`#${target.id.match(/^.*?(?=-)/)}`));
+		}
+	});
+};
+
+
+const observer = new IntersectionObserver(scrollObserverCallback, {
+	root: document.querySelector('#app'),
+	rootMargin: '0px',
+	threshold: 1.0
+  });
+
+document.querySelectorAll('.Title').forEach(button => {
+	observer.observe(document.querySelector(`#${button.id}-content`));
+	button.addEventListener('click', () => {
+		switchContent(event.target);
+	});
 });
