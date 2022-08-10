c937734f3e8b580736851d4e34956d8cc89acdad
David Duefrene <fortismilite@gmail.com>
2022-08-10 00:49:57 -0600

    added alt text, fix color contrast

--- theme.js
+++ theme.js
@@ -2,20 +2,21 @@ import ThemeController from 'themecontroller'
 
 const myThemes = {
 	'light': {
-		'text-color': 'black',
+		'text-color': '#444444',
 		'background-color': '#e3e3e3',
-		'off-background-color': '#f5f5f5',
+		'off-background-color': '#dbdada',
 		'accent-color': '#0eebc659',
 		'highlight-color': '#6b0505',
 		'glass-color': 'hsla(215.8, 87.3%, 33.9%, 0.05)',
 	},
 	'dark': {
-		'text-color': 'white',
-		'background-color': '#202020',
-		'off-background-color': '#2f3136',
+		'text-color': '#e3e3e3',
+		'background-color': '#444444',
+		'off-background-color': '#575757',
 		'accent-color': '#0eebc659',
 		'highlight-color': '#f1c40f',
-		'glass-color': 'hsla(215.8, 87.3%, 33.9%, 0.05)',
+		// 'glass-color': 'hsla(215.8, 87.3%, 33.9%, 0.05)',
+		'glass-color': '#44444424',
 	},
 }
 
