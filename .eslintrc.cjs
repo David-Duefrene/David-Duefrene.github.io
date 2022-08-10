201b4f5b334cc06ffb09f1312a2901f46b535c3c
David Duefrene <fortismilite@gmail.com>
2022-08-10 00:25:32 -0600

    Moved all colors into ThemeController

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 // eslint-disable-next-line no-undef
 module.exports = {
 	extends: [ 'vigilant-octo-train' ],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+	},
 }
--- theme.js
+++ theme.js
@@ -1,17 +1,22 @@
-import ThemeController from 'themecontroller';
+import ThemeController from 'themecontroller'
 
 const myThemes = {
 	'light': {
-		'main-color': 'black',
-		'off-main-color': '#4d5155',
-		'background-color': 'white',
+		'text-color': 'black',
+		'background-color': '#e3e3e3',
+		'off-background-color': '#f5f5f5',
+		'accent-color': '#0eebc659',
+		'highlight-color': '#6b0505',
+		'glass-color': 'hsla(215.8, 87.3%, 33.9%, 0.05)',
 	},
 	'dark': {
-		'main-color': 'white',
-		'off-main-color': '#c2d0df',
-		'background-color': 'black',
+		'text-color': 'white',
+		'background-color': '#202020',
+		'off-background-color': '#2f3136',
+		'accent-color': '#0eebc659',
+		'highlight-color': '#f1c40f',
+		'glass-color': 'hsla(215.8, 87.3%, 33.9%, 0.05)',
 	},
 }
 
-// const MyThemeController = ThemeController(theme = 'OceanBlue', myThemes)
 window.ThemeController = new ThemeController(myThemes)
