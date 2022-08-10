cba338b3d435541e19e3a0f438396e48c8a0329a
David Duefrene <fortismilite@gmail.com>
2022-08-09 22:36:17 -0600

    Added ESLint config

--- switchContent.js
+++ switchContent.js
@@ -1,39 +1,38 @@
 const switchContent = (target) => {
-	document.querySelector('.Title.Nav-button.Active').classList.remove('Active');
+	document.querySelector('.Title.Nav-button.Active').classList.remove('Active')
 	target.classList.add('Active')
 }
 
 const scrollObserverCallback = (entries) => {
-	entries.forEach(entry => {
+	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
-			const target = entry.target;
-			switchContent(document.querySelector(`#${target.id.match(/^.*?(?=-)/)}`));
+			const target = entry.target
+			switchContent(document.querySelector(`#${target.id.match(/^.*?(?=-)/)}`))
 		}
-	});
-};
-
+	})
+}
 
 const observer = new IntersectionObserver(scrollObserverCallback, {
 	root: document.querySelector('#app'),
 	rootMargin: '0px',
-	threshold: 1.0
-  });
+	threshold: 1.0,
+})
 
-document.querySelectorAll('.Title').forEach(button => {
-	if (button.id == 'ThemeSwitcher') {
+document.querySelectorAll('.Title').forEach((button) => {
+	if (button.id === 'ThemeSwitcher') {
 		// Capitalize first letter of string
-		const capitalize = (string,) => {
-			return string.charAt(0,).toUpperCase() + string.slice(1,)
+		const capitalize = (string) => {
+			return string.charAt(0).toUpperCase() + string.slice(1)
 		}
 		button.addEventListener('click', () => {
-			window.ThemeController.theme = window.ThemeController.theme == 'light' ? 'dark' : 'light';
-			button.innerHTML = capitalize(window.ThemeController.theme);
-		});
-		button.innerHTML = capitalize(window.ThemeController.theme);
-		return;
+			window.ThemeController.theme = window.ThemeController.theme === 'light' ? 'dark' : 'light'
+			button.innerHTML = capitalize(window.ThemeController.theme)
+		})
+		button.innerHTML = capitalize(window.ThemeController.theme)
+		return
 	}
-	observer.observe(document.querySelector(`#${button.id}-content`));
+	observer.observe(document.querySelector(`#${button.id}-content`))
 	button.addEventListener('click', () => {
-		document.querySelector(`#${button.id}-content`).scrollIntoView(false);
-	});
-});
+		document.querySelector(`#${button.id}-content`).scrollIntoView(false)
+	})
+})
