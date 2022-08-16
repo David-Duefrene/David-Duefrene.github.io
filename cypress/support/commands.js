6b593d7ec9d6a5463329106970f76a3b63d98e90
David Duefrene <fortismilite@gmail.com>
2022-08-15 18:19:23 -0600

    implement basic snapshot testing

---
+++ cypress/support/commands.js
@@ -0,0 +1,4 @@
+// For a comprehensive list of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+require('@cypress/snapshot').register()
---
+++ cypress/support/e2e.js
@@ -0,0 +1,2 @@
+// Cypress config docs: https://on.cypress.io/configuration
+import './commands'
