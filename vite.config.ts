961afc940f82d23195c3c22537ade07090dfb2fa
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-07-25 20:38:03 -0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		lib: {
-			entry: './components/index.js',
+			entry: './index.html',
 			name: 'web-components',
 			fileName: (format) => `web-components.${format}.js`,
 		  },
 		outDir: './docs',
+
 	},
 });
